8260e34d60a857ecdcce9aaadb1a557c8d7abc14
rohitnikat19 <139055818+rohitnikat19@users.noreply.github.com>
2025-09-21 12:40:06 +0530

    Create Program10.java

---
+++ 1/Program10.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class RevNum {
+
+        int num;
+        int rev;
+
+        RevNum(int num){
+
+                this.num = num;
+        }
+
+        int getReverseNum() {
+
+                while(num > 0){
+
+                        int rem = num % 10;
+                        rev = rev * 10 + rem;
+                        num = num / 10;
+                }
+                return rev;
+
+        }
+
+}
+
+class Client {
+
+        public static void main(String[] args) {
+
+                Scanner sc = new Scanner(System.in);
+
+                System.out.println("Enter the Number");
+                int num = sc.nextInt();
+
+                RevNum revNum = new RevNum(num);
+
+                int temp = revNum.getReverseNum();
+
+                if(temp == num){
+
+                        System.out.println("Is palindrome");
+                } else {
+
+                        System.out.println("Is not palindrome");
+                }
+
+        }
+}
